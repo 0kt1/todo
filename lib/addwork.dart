e7729e0b708eb09c09dbf3ecb9341a3be3c1d049
kaustubh <kaustubhutitare@gmail.com>
2023-07-07 20:22:09 +0530

    2ndok

--- lib/addwork.dart
+++ lib/addwork.dart
@@ -26,7 +26,9 @@ class _AddWorkState extends State<AddWork> {
               'Add Work',
             style: TextStyle(
               fontFamily: 'Montserrat',
-              fontWeight: FontWeight.bold,
+              fontWeight: FontWeight.bold
+
+              ,
             ),
           ),
           centerTitle: true,
